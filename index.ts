072600adaf41ff99cdb8caeac87c14762d47e680
dario <darioyzf@gmail.com>
2016-06-17 12:40:44 +0200

    oki

--- index.ts
+++ index.ts
@@ -209,7 +209,7 @@ export = function cli() {
             })
 
             async.eachSeries(platforms, function (pla, cb) {
-              console.log("adding platform " + pla);
+              console.log("adding platform " + pla+ "in "+dir);
               exec("cd " + dir + " && cordova platform add " + pla + " --save").then(function () {
                 cb();
               }).catch(function (err) {
