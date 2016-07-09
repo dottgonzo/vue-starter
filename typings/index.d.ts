fe9cc1b01b8d81e2403ba460813f994aac3eafb8
Dario Caruso <darioyzf@gmail.com>
2016-07-09 05:02:10 +0200

    up

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,5 @@
+/// <reference path="globals/async/index.d.ts" />
+/// <reference path="globals/commander/index.d.ts" />
+/// <reference path="globals/git-config/index.d.ts" />
+/// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="modules/bluebird/index.d.ts" />
---
+++ vuekit/staticserver.js
@@ -0,0 +1,1 @@
+require('http').createServer(require('serve-static')('./platforms/browser/www')).listen(8081);
