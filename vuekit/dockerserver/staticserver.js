f46a97f7dd5dc44244ee37dfad9a73db2ce63e43
Dario Caruso <darioyzf@gmail.com>
2016-07-30 02:19:10 +0200

    fix

--- vuekit/dockerserver/staticserver.js
+++ vuekit/dockerserver/staticserver.js
@@ -1,7 +1,6 @@
 var finalhandler = require('finalhandler')
 var http = require('http')
 var serveStatic = require('serve-static')
-var conf = require('./staticserver.json')
 
 // Serve up public/ftp folder
 var serve = serveStatic('./', {'index': ['index.html']})
